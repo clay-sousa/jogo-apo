c0fc89fe330384f5635f254d45eb67336d5839ca
clay-sousa <clayson.sousa@gmail.com>
2021-10-29 09:58:01 -0300

    adicao menu principal

--- forca/forca.c
+++ forca/forca.c
@@ -1,10 +1,37 @@
 #include<stdio.h>
 #include<locale.h>
 
+int menu;
+
 int main (void) {
 	setlocale (LC_ALL, "Portuguese");
 	
 	printf("Jogo: A Forca");
+	printf("\nMenu Principal");
+	printf("\n1. Sobre o jogo");
+	printf("\n2. Jogar");
+	printf("\n3. Finalizar");
+	printf("\nDigite a opção desejada: ");
+	scanf("%d", &menu);
+	
+	if (menu==1){
+		printf("\nSobre o Jogo:");
+	}
+	else
+		if(menu==2){
+			printf("\nJogar");
+		}
+		else
+			if(menu==3){
+				printf("\nFinalizando...");
+				printf("\nObrigado por jogar :D");
+			}
+			else{
+				printf("\nComando Inválido...");
+				printf("\nSelecione uma opção válida.");
+			}
+	
+	
 	
 	return 0;
 }
