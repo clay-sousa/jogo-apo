c0d431add24b1ad15e49610be956c8951323e9f0
clay-sousa <clayson.sousa@gmail.com>
2021-10-29 21:12:00 -0300

    menu com funções

--- forca/forca.c
+++ forca/forca.c
@@ -1,37 +1,77 @@
 #include<stdio.h>
 #include<locale.h>
+#include<time.h>
 
-int menu;
-
-int main (void) {
-	setlocale (LC_ALL, "Portuguese");
-
-	printf("Jogo: A Forca");
-	printf("\nMenu Principal");
+void menu() {
+	printf("Jogo: A Forca\n\n");
+	printf("Menu Principal\n");
 	printf("\n1. Sobre o jogo");
 	printf("\n2. Jogar");
-	printf("\n3. Finalizar");
+	printf("\n3. Finalizar\n");
 	printf("\nDigite a opção desejada: ");
-	scanf("%d", &menu);
+}
 
-	if (menu==1){
-		printf("\nSobre o Jogo:");
+char escolha_user() {
+	char op;
+	int valido = 0;
+	while(valido == 0) {
+		scanf(" %c", &op);
+		switch(op){
+			case '1':	
+			case '2':	
+			case '3':
+				system("cls");
+				return op;
+			default:
+				printf("Opção Invalida...\n\n");
+				printf("\nCarregando...");
+				sleep(2);
+				system("cls");
+				menu();	
+		}
+	}	
+}
+
+int sobre_jogo() {
+	printf("\n\nCarregando...");
+	sleep(3);
+	system("cls");
+	printf("Sobre o Jogo");
+}
+
+int jogar() {
+	printf("\n\nCarregando...");
+	sleep(3);
+	system("cls");
+	printf("Jogar");
+}
+
+int fim() {
+	printf("\n\nCarregando...");
+	sleep(3);
+	system("cls");
+	printf("Finalizando");
+}
+
+int main (){
+	setlocale(LC_ALL, "Portuguese");
+	char option;
+	
+	menu();
+	option = escolha_user();
+	
+	printf("A opção escolhida foi %c", option);
+	
+	if(option == '1'){
+		sobre_jogo();
 	}
 	else
-		if(menu==2){
-			printf("\nJogar");
+		if(option == '2'){
+			jogar();
 		}
-		else
-			if(menu==3){
-				printf("\nFinalizando...");
-				printf("\nObrigado por jogar :D");
-			}
-			else{
-				printf("\nComando Inválido...");
-				printf("\nSelecione uma opção válida.");
-			}
-
-
-
-	return 0;
+		else{
+			fim();
+		}
+		
+	return 0;	
 }
